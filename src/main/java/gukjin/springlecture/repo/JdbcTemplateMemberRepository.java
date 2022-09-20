2c7cc8e64c64aa52af2c22969581ab1f093fde00
=gukjin <internet2dot0@gmail.com>
2022-09-20 18:53:38 +0900

    Feat: SpringJpaData and test done

--- src/main/java/gukjin/springlecture/repo/JdbcTemplateMemberRepository.java
+++ src/main/java/gukjin/springlecture/repo/JdbcTemplateMemberRepository.java
@@ -2,13 +2,17 @@ package gukjin.springlecture.repo;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 import gukjin.springlecture.domain.Member;
 
@@ -23,41 +27,53 @@ public class JdbcTemplateMemberRepository implements MemberRepository {
 
 	@Override
 	public Member save(Member member) {
-		return null;
-	}
+		SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+		jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+		
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("name", member.getName());
+		
+		Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+		member.setId(key.longValue());
+		return member;
+	} 
 	
-	/*
-	private RowMapper<Member> memberRowMapper(){
-		return (rs, rowNum) -> {
-			Member member = new Member();
-			member.setId(rs.getLong("id"));
-			member.setName(rs.getString("name"));
-			return member;
+	private RowMapper<Member> memberRowMapper() {
+		return new RowMapper<Member>() {
+			@Override
+			public Member mapRow(ResultSet rs, int Rownum) throws SQLException{
+				Member member = new Member();
+				member.setId(rs.getLong("id"));
+				member.setName(rs.getString("name"));
+				return member;
+			}
 		};
 	}
-	*/
+	
+//	private RowMapper<Member> memberRowMapperLambda(){
+//		return (rs, rowNum) -> {
+//			Member member = new Member();
+//			member.setId(rs.getLong("id"));
+//			member.setName(rs.getString("name"));
+//			return member;
+//		};
+//	}
 	
 	@Override
 	public Optional<Member> findById(Long id) {
-		List<Member> result = jdbcTemplate.query("select * from member where id = ?", (rs, rowNum) -> {
-			Member member = new Member();
-			member.setId(rs.getLong("id"));
-			member.setName(rs.getString("name"));
-			return member;
-		});
+		List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper(), id);
 		return result.stream().findAny();
 	}
 
 	@Override
 	public Optional<Member> findByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
+		return result.stream().findAny();
 	}
 
 	@Override
 	public List<Member> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return jdbcTemplate.query("select * from member where id = ?", memberRowMapper());
 	}
 
 }
--- src/test/java/gukjin/springlecture/service/MemberServiceIntegrationTest.java
+++ src/test/java/gukjin/springlecture/service/MemberServiceIntegrationTest.java
@@ -42,7 +42,7 @@ class MemberServiceIntegrationTest {
 		
 		
 		IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
-		assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다1");			
+		assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");			
 	}
 
 }
